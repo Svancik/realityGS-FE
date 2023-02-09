d315c0607481a5ed8a9a9a25ffdabda9b416d4ce
Svancik <thesvancik@gmail.com>
2023-02-09 21:55:28 +0100

    realityTable

--- src/pages/makler/Makler.jsx
+++ src/pages/makler/Makler.jsx
@@ -5,12 +5,14 @@ import { RealityTable } from "../../components/realityTable/RealityTable";
 import { Agent } from "./../../components/agent/Agent";
 import { Footer } from "../../components/footer/Footer";
 
-
 export const Makler = () => {
   return (
     <div className="maklerWrapper">
       <Topbar />
       <Agent />
+      {/* <div className="typeHeader">
+        <h2>Nemovitosti</h2>
+      </div> */}
       <RealityTable />
       <Footer />
     </div>
