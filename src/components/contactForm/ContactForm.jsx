de4c994d393418018950ae8a9dd480c4922a40fc
Svancik <thesvancik@gmail.com>
2023-02-21 22:36:34 +0100

    contactForm + investors finished

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -5,10 +5,55 @@ export const ContactForm = () => {
   return (
     <div className="contactFormWrapper">
       <form action="">
-        <label htmlFor="name">Jméno a příjmení</label>
-        <input type="text" id="name" />
+        <div className="form-row">
+          <div className="left">
+            <label htmlFor="name">Jméno a příjmení</label>
+          </div>
+          <input type="text" id="name" />
+        </div>
 
-        <input type="email" id="email" />
+        <div className="form-row">
+          <div className="left">
+            <label htmlFor="email">Email</label>
+          </div>
+          <input type="text" id="email" placeholder="@" />
+        </div>
+
+        <div className="form-row">
+          <div className="left">
+            <label htmlFor="email">Telefonní číslo</label>
+          </div>
+          <input type="number" id="number" placeholder="+420" />
+        </div>
+
+        <div className="form-row">
+          <div className="left">
+            <label htmlFor="email">Zvolená služba</label>
+          </div>
+          <select name="cars" id="cars">
+            <option value="volvo">Hledám investiční nemovitost</option>
+            <option value="saab">Hledám rekonstrukci nemovitosti</option>
+            <option value="opel">Hledám správu nemovitosti</option>
+            <option value="audi">Jiné</option>
+          </select>
+        </div>
+
+        <div className="form-row">
+          <div className="left">
+            <label htmlFor="email">Zpráva</label>
+          </div>
+          <textarea
+            rows="10"
+            type="text"
+            id="text"
+            placeholder="Zanechte nám vaši zprávu."
+            size="50"
+          />
+        </div>
+
+        <div className="form-row">
+          <button className="poptavka">NEZÁVAZNĚ POPTAT</button>
+        </div>
       </form>
     </div>
   );
